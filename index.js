897c27cbb89a72cebb6ce5e1c278c8ecbaf14376
Dilan Silva <sdilansilva@gmail.com>
2021-03-16 17:51:51 +0530

    Colombo route is done but have to done more

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 4000;//port
 
 const mongodb = require('mongodb');//mongodb package
-const bcrypt = require('bcrypt');//for hashin
+const bcrypt = require('bcrypt');//for hashing
+const request = require('request');//request
 
 const MongoClient = mongodb.MongoClient;//initialize the connection
 const connectionURL = 'mongodb://127.0.0.1:27017';//database server connection
@@ -108,7 +109,10 @@ app.post('/emailverify',async (req,res) => {//email verification route
 });
 
 app.get('/colombo',(req,res) => {
-    
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=6.927079&lon=79.861244&exclude=current&appid=5c301eacf55ba92acebe2c2d52aebe38';
+    request({url:url,json : true}, function (error, response, body) {
+    console.log(body.hourly[0].weather); // Print the HTML for the Google homepage.
+});
 });
 
 app.listen(port, () => {
