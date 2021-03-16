9b04474e69680a00f0b7c7a301e531510061ca36
Dilan Silva <sdilansilva@gmail.com>
2021-03-16 10:20:16 +0530

    hashed the password

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 4000;//port
 const mongodb = require('mongodb');//mongodb package
 const bcrypt = require('bcrypt');//for hashin
 
+const MongoClient = mongodb.MongoClient;//initialize the connection
+const connectionURL = 'mongodb://127.0.0.1:27017';//database server connection
+const databaseName = 'Loons-Lab';//Database name
+
 app.use(express.json());//json parser
 
 app.post('/login', (req, res) => {
@@ -12,8 +16,19 @@ app.post('/login', (req, res) => {
   res.send('Hello World');
 })
 
-app.post('/signup', (req, res) => {
-    console.log(req.body);
+app.post('/signup', async (req, res) => {
+
+   try {
+        //salt and hashed the password
+        const salt = await bcrypt.genSalt(10);//generate a 10 salt
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);//hash the password with salt
+        
+        MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error,client) => {
+            
+        })
+   } catch (error) {
+    res.status(500).send();
+   }
 });
 
 app.listen(port, () => {
